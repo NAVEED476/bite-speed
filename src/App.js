4c89b838010dbef0eb9c34841a03357195f91d2c
Naveed Shaik <shaiknaveed435@gmail.com>
2024-05-26 02:28:56 +0530

    basic react flow created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import RightBar from './components/RightBar';
+import Flow from "./pages/Flow"
 
 function App() {
   return (
     <div className="App">
      <h1>Hi</h1>
+     <Flow/>
     </div>
   );
 }
---
+++ src/inital-nodes.js
@@ -0,0 +1,39 @@
+import { MarkerType } from "reactflow";
+
+export const nodes = [
+  {
+    id: "1",
+    type: "node",
+    data: { heading: "Send Message", content: "This is text 1" },
+    position: { x: 50, y: 200 },
+  },
+];
+
+export const edges = [
+  {
+    id: "e1-2",
+    source: "1",
+    target: "2",
+    label: "this is an edge label",
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+];
+
+
+export function isAllNodeisConnected(nodes, edges) {
+    console.log(nodes, edges, "is");
+    const allNodesIds = nodes.map((node) => node.id);
+    const allSourceEdges = edges.map((edge) => edge.source);
+    let count = 0;
+    for (let i = 0; i < allNodesIds.length; i++) {
+      if (!allSourceEdges.includes(allNodesIds[i])) count++;
+    }
+    console.log(allNodesIds, allSourceEdges);
+    if (count >= 2) {
+      return false;
+    }
+    return true;
+  }
+  
